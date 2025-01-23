b5953f2b343b803c12494966edb7d7736fcc9e82
ceci090 <22300124@uttt.edu.mx>
2025-01-22 18:25:53 -0600

    CSS 2

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Servir el archivo style.css desde la raíz del proyecto
 app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, 'styles', 'style.css'));
+  res.sendFile(path.join(__dirname, 'style.css'));
 });
 
 // Servir el archivo index.html desde la raíz
