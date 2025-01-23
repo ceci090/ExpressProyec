2c8af2c5b1ab286f320bb99801aba680704d5c18
ceci090 <22300124@uttt.edu.mx>
2025-01-22 18:30:17 -0600

    CSS 3

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Servir el archivo style.css desde la raíz del proyecto
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, 'style.css'));
-});
+// Servir archivos estáticos desde la raíz del proyecto (como style.css)
+app.use(express.static(path.join(__dirname)));
 
 // Servir el archivo index.html desde la raíz
 app.get('/', (req, res) => {
@@ -17,3 +15,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
+
